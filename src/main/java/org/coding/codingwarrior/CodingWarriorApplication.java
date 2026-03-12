39a63a75e782721bd872ca2d374277c85b802717
Nagesha S <nageshasriram@gmail.com>
2026-03-12 08:58:10 +0530

    initial commit

---
+++ src/main/java/org/coding/codingwarrior/CodingWarriorApplication.java
@@ -0,0 +1,13 @@
+package org.coding.codingwarrior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingWarriorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingWarriorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/coding/codingwarrior/CodingWarriorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.coding.codingwarrior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingWarriorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
